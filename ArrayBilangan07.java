d74a860eb9e52da0f10f172f52a1c971b04c2ee4
bimaadiwijaya <bimaadiwijaya83@gmail.com>
2024-10-31 14:36:41 +0700

    percobaan 1

---
+++ ArrayBilangan07.java
@@ -0,0 +1,10 @@
+public class ArrayBilangan07 {
+    public static void main(String[] args) {
+        int[] bil = new int[4];
+
+        System.out.println(bil[0]);
+        System.out.println(bil[1]);
+        System.out.println(bil[2]);
+        System.out.println(bil[3]);
+    }
+}
